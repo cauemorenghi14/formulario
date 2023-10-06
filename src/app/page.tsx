7918761740c342f1595e726113462d6a194b9347
Cauê Morenghi Lopes <caue.lopes@quaestum.com.br>
2023-10-06 15:38:07 -0300

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,76 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useForm } from "react-hook-form";
+import * as S from "./styles";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function Home() {
+const schemaForm = z.object({
+  name: z
+    .string()
+    .nonempty('o campo "nome" não pode estar vazio')
+    .toUpperCase(),
+  email: z
+    .string()
+    .nonempty('o campo "email" não pode estar vazio')
+    .email("escreva um email com formato válido")
+    .toLowerCase()
+    .refine(email => {
+      return email.endsWith('gmail.com')
+    }, 'o email deve terminar com "gmail.com"'),
+  password: z
+    .string()
+    .nonempty("o campo senha é obrigatório")
+    .min(6, "a senha deve ter no mínimo 6 caracteres")
+});
+
+type FormProps = z.infer<typeof schemaForm>;
+
+const Home = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormProps>({
+    mode: "all",
+    resolver: zodResolver(schemaForm),
+  });
+
+  const handleSubmitData = (data: FormProps) => {
+    console.log(data);
+  };
+
+  console.log(errors);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <S.Container>
+      <S.Form action="" onSubmit={handleSubmit(handleSubmitData)}>
+        <label htmlFor="">Nome</label>
+        <input type="text" {...register("name")} />
+        {errors.name && <span>{errors.name.message}</span>}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <label htmlFor="">Email</label>
+        <input type="email" {...register("email")} />
+        {errors.email && <span>{errors.email.message}</span>}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <label htmlFor="">Senha</label>
+        <input type="password" {...register("password")} />
+        {errors.password && <span>{errors.password.message}</span>}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <button>Salvar</button>
+      </S.Form>
+    </S.Container>
+  );
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+export default Home;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+// 1 - criar formulário (inputs, labels e button)
+// 2 - criar useForm, importar o register, handleSubmit e formState: {errors} e colocar mode: 'all' dentro do useForm, e criar funçao handleSubmitData (data: any) e dar um console.log na data
+// 3 - colocar {...register('nome do campo')} nos inputs
+// 4 - testar se tá indo no console.log quando envia qq coisa
+// 5 - criar schema com os mesmos nomes dos inputs e criar validações que quiser
+// 6 - criar type FormProps e inferir (z.infer<>) do mesmo tipo do schema: z.infer<typeof schema>
+// 7 - passar o type FormProps para o useState e a data no handleSubmitData
+// 8 - colocar "resolver": zodResolver(schema) para começar a funcionar as validações do schema
+// 9 - dar console.log(errors) para ver os erros
+// 10 - exibir os erros abaixo dos inputs com um span: {errors.nomedocampo && <span>{errors.nomedocampo.message}</span>}
---
+++ src/app/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+`
